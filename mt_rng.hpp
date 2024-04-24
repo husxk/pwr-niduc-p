9bb637c2d3c0037369cad95b9dc42f11fe797a11
Jakub Toborek <272979@student.pwr.edu.pl>
2024-04-24 16:50:49 +0200

    Implementation of get_normal() and creation of a tool to visualize normal distributions for get_normal() and get_next()

--- mt_rng.hpp
+++ mt_rng.hpp
@@ -1,10 +1,10 @@
 #ifndef MT_RNG_H
 #define MT_RNG_H
 
-#include <stdint.h>
+#include <cstdint>
 
 void init_mt(uint32_t x0);
-uint32_t mersenne_twister();
 uint32_t get_next(uint32_t min, uint32_t max);
+uint32_t get_normal(double mean, double standard_deviation);
 
 #endif // MT_RNG_H
