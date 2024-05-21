1993e55b12004e97aa2e5815a14ae1e1c31fe8d8
Maciej Pacholczyk <mpacholczyk31s@gmail.com>
2024-05-21 08:41:07 +0200

    Implement whitening

--- data_process.cpp
+++ data_process.cpp
@@ -38,3 +38,25 @@ data_process::check_crc(uint8_t* data, size_t size)
   crc_calc(data, size);
   return crc == 0;
 }
+
+
+// ref:
+// https://www.allaboutcircuits.com/technical-articles/whitening-filters-help-low-power-radios-tackle-issues-caused-by-long-identical-bit-sequences/
+//
+// https://www.allaboutcircuits.com/technical-articles/long-distance-bluetooth-low-energy-bit-data-paths/
+
+void
+data_process::whitening(uint8_t* data, size_t size)
+{
+  whitening_register reg;
+
+  for(size_t i = 0; i < size; i++)
+  {
+    for(size_t j = 0; j < 8; j++)
+    {
+      (*data) ^= reg.get_bit(j);
+      reg.cycle();
+    }
+    data++;
+  }
+}
--- data_process.hpp
+++ data_process.hpp
@@ -12,12 +12,43 @@ class data_process
   static constexpr uint32_t crc_mask_result = (1 << crc_size) - 1;
   static constexpr uint32_t crc_init_value  =  0;
 
+  static constexpr uint32_t whitening_init  =  0x5A;
+
   uint32_t crc;
+
+  class whitening_register
+  {
+    uint8_t as_unsigned;
+
+    public:
+
+    uint8_t get_bit(uint8_t x)
+    {
+      return as_unsigned & (1 << x);
+    }
+
+    void cycle()
+    {
+      as_unsigned <<= 1;
+
+      const uint8_t msb_bit = as_unsigned >> 7;
+
+      as_unsigned ^= msb_bit << 4;
+      as_unsigned |= msb_bit;
+    }
+
+    whitening_register()
+    {
+      this->as_unsigned = whitening_init;
+    }
+  };
 
   void crc_calc(uint8_t*, size_t);
 
   public:
     bool check_crc(uint8_t*, size_t);
     uint32_t generate_crc(uint8_t*, size_t);
+
+    void whitening(uint8_t*, size_t);
 
 };
